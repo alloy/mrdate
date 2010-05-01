43fd3c9599575baea9759f77aebc6a1b8fac7fa5
Eloy Duran <eloy.de.enige@gmail.com>
2010-05-01 15:16:02 +0200

    create lib skeleton and make the Date rubyspecs run with Bacon

---
+++ Rakefile
@@ -0,0 +1,10 @@
+SPECS = %w{ new }
+SPEC_COMMAND = "macruby -r #{SPECS.map { |s| "spec/#{s}_spec.rb" }.join(" -r ")} -e \"\""
+
+task :spec do
+  sh SPEC_COMMAND
+end
+
+task :kick do
+  sh "kicker -e '#{SPEC_COMMAND}' ."
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,40 @@
+require "rubygems"
+require "bacon"
+
+Bacon.summary_on_exit
+
+class Bacon::Context
+  def it_behaves_like(like, method)
+    @method = method
+    behaves_like(like)
+  end
+  
+  def not_compliant_on(platform)
+    yield unless platform == :macruby
+  end
+  
+  def raise_error(type)
+    lambda do |block|
+      begin
+        block.should.raise(type)
+        true
+      rescue Bacon::Error
+        false
+      end
+    end
+  end
+end
+
+def describe(name, options = {}, &block)
+  if options[:shared]
+    shared(name, &block)
+  else
+    super
+  end
+end
+
+class Object
+  def should_not(result)
+    should.not result
+  end
+end
