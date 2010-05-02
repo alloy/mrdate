6c01c48feb546f92728142b1c3e4353ad82da3c2
Eloy Duran <eloy.de.enige@gmail.com>
2010-05-02 17:51:58 +0200

    Write actual #=== specs, we pass them already

--- spec/relationship_spec.rb
+++ spec/relationship_spec.rb
@@ -1,20 +1,19 @@
-require 'date' 
-require File.expand_path('../../../spec_helper', __FILE__)
+require File.expand_path('../spec_helper', __FILE__)
 
 describe "MRDate#===" do
 
   it "should be able to compare two same dates" do
-    (MRDate.civil(2000, 04, 06) <=> MRDate.civil(2000, 04, 06)).should == 0
-  end
-
-  it "should be able to compute the difference between two dates" do
-    (MRDate.civil(2000, 04, 05) <=> MRDate.civil(2000, 04, 06)).should == -1
-    (MRDate.civil(2001, 04, 05) <=> MRDate.civil(2000, 04, 06)).should == 1
+    (MRDate.civil(2000, 04, 06) === MRDate.civil(2000, 04, 06)).should == true
+    (MRDate.civil(2000, 04, 06) === MRDate.civil(2000, 04, 07)).should == false
   end
 
   it "should be able to compare to another numeric" do
-    (MRDate.civil(2000, 04, 05) <=> MRDate.civil(2000, 04, 06).jd).should == -1
-    (MRDate.civil(2001, 04, 05) <=> MRDate.civil(2000, 04, 06).jd).should == 1
+    (MRDate.civil(2000, 04, 06) === MRDate.civil(2000, 04, 06).jd).should == true
+    (MRDate.civil(2000, 04, 06) === MRDate.civil(2000, 04, 07).jd).should == false
   end
 
+  it "returns false for any non numeric or dates" do
+    (MRDate.civil(2000, 04, 06) === "2000-04-06").should == false
+    (MRDate.civil(2000, 04, 06) === :"2000-04-06").should == false
+  end
 end
