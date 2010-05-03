3ab2e1bb38b6b4423f5b115538b6b215da2f9017
Eloy Duran <eloy.de.enige@gmail.com>
2010-05-03 17:04:08 +0200

    Make #dup work, but we skip extra TZ flags for now

---
+++ spec/time/dup_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path('../../spec_helper', __FILE__)
+require File.expand_path('../fixtures/methods', __FILE__)
+
+describe "MRTime#dup" do
+  it "returns a MRTime object that represents the same time" do
+	  t = MRTime.at(100)
+	  t.dup.tv_sec.should == t.tv_sec
+  end
+
+  # it "copies the gmt state flag" do
+  #     MRTime.now.gmtime.dup.gmt?.should == true
+  # end
+
+  # it "returns an independent MRTime object" do
+  #     t = MRTime.now
+  #     t2 = t.dup
+  #     t.gmtime
+  # 
+  #     t2.gmt?.should == false
+  # end
+end
